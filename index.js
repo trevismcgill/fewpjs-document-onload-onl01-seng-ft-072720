e0e30732db3175f0d0c21b9a714201e646a2988c
trevismcgill <trevismcgill25@gmail.com>
2020-10-29 14:15:06 -0500

    commit tree b2e87670818a2894e6c1a65c9634012f8b9c4f7b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    debugger
+   replaceText();
+});
+
+function replaceText() {
+    document.querySelector("#text").innerHTML = "This is really cool!";
+};
